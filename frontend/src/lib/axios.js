b454fc14a757b2c8339d3edd5ac1bbad32adb37d
Anjali-pandey11 <pandeyanjali7906@gmail.com>
2026-04-02 20:33:06 +0530

    fixes-2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:import.meta.env.VITE_API_URL,
+  baseURL:import.meta.env.VITE_API_URL || "https://video-calling-interview-platform-2-ib2q.onrender.com/api",
   withCredentials:true // browser send the cookies to server automatically at every aingle request
 })
 
